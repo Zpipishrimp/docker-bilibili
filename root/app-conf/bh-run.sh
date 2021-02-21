720ff360b728e289651ec4515ead72d11511f0da
Wei Ren <weiren2@illinois.edu>
2021-02-21 13:50:36 -0600

    Updated scripts to use Telegram bot notification.

--- root/app-conf/bh-run.sh
+++ root/app-conf/bh-run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/with-contenv bash
 
 # 启动bilibili-helper
-cd /app && java -jar /app/BILIBILI-HELPER.jar ${DEDEUSERID} ${SESSDATA} ${BILI_JCT} ${SERVERPUSHKEY} | s6-setuidgid abc tee /config/bilibili-helper.log
+cd /app && java -jar /app/BILIBILI-HELPER.jar ${DEDEUSERID} ${SESSDATA} ${BILI_JCT} ${SERVERPUSHKEY} ${TELEGRAMBOTTOKEN} ${TELEGRAMCHATID} | s6-setuidgid abc tee /config/bilibili-helper.log
