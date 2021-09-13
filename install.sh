683f20b9220548bfb3f6b6acc367ce12adbed9a6
SuperNG6 <sleele@gmail.com>
2021-09-13 15:56:18 +0800

    提交 `2.0.0 Preview`版
    提交 2.0.0 Preview版
    
    Update Dockerfile
    
    提交 `2.0.0 Preview`版

--- install.sh
+++ install.sh
@@ -3,9 +3,9 @@
 S6_VER=2.1.0.2
 BHV=$(cat ReleaseTag | head -n1)
 # download bilibili-helper
-wget --no-check-certificate https://github.com/JunzhouLiu/BILIBILI-HELPER-PRE/releases/download/v${BHV}/BILIBILI-HELPER-v${BHV}.zip
-unzip BILIBILI-HELPER-v${BHV}.zip
-mv BILIBILI-HELPER-v${BHV}.jar BILIBILI-HELPER.jar
+# wget --no-check-certificate https://github.com/JunzhouLiu/BILIBILI-HELPER-PRE/releases/download/v${BHV}/BILIBILI-HELPER-v${BHV}.zip
+# unzip BILIBILI-HELPER-v${BHV}.zip
+# mv BILIBILI-HELPER-v${BHV}.jar BILIBILI-HELPER.jar
 # download s6-overlay
 # Check CPU architecture
 ARCH=$(uname -m)
