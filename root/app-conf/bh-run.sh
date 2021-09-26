1c61fb54360a47a451f4bcbd30a052ec55cc740c
SuperNG6 <sleele@gmail.com>
2021-09-26 23:00:20 +0800

    修复`-e cron=random`在某些情况下可能会在一天内重复执行的问题
    Update random-bh.sh
    
    Update random-bh.sh
    
    Update Manual Build Docker Image.yml
    
    Revert "Update Manual Build Docker Image.yml"
    
    This reverts commit 1bb239aff2ce1045866a3623406eb572a85201d4.
    
    Update bh-run.sh

--- root/app-conf/bh-run.sh
+++ root/app-conf/bh-run.sh
@@ -1,11 +1,20 @@
 #!/usr/bin/with-contenv bash
-
 # 启动bilibili-helper
-cd /app && java -jar BILIBILI-HELPER.jar /config/config.json | s6-setuidgid abc tee /config/bilibili-helper.log
+BH_TODAY="$(date +"%Y-%m-%d")"
+BH_RUN_TIME="$(grep ^今天已签到完毕 "/config/bilibili-helper.log" | cut -d: -f2-)"
 
-if [ "${CRON}" = random ]
+if [ "${BH_TODAY}" != "${BH_RUN_TIME}" ] && [ "${CRON}" = random ]
 then
+    cd /app && java -jar BILIBILI-HELPER.jar /config/config.json | s6-setuidgid abc tee /config/bilibili-helper.log
+    echo -e "今天已签到完毕:$(date +"%Y-%m-%d")" | s6-setuidgid abc tee -a /config/bilibili-helper.log
     . /app-conf/random-bh.sh
-    ps -ef | grep cron | grep -v grep | awk '{printf $2}' | xargs kill -9
-    ps -ef | grep tail | grep -v grep | awk '{printf $2}' | xargs kill -9
+    crontab /config/bh-crontab
+    echo -e "明天签到的时间为:" | s6-setuidgid abc tee -a /config/bilibili-helper.log
+    cat /config/bh-crontab | s6-setuidgid abc tee -a /config/bilibili-helper.log
+fi
+
+if [ "${CRON}" = true ]
+then
+    cd /app && java -jar BILIBILI-HELPER.jar /config/config.json | s6-setuidgid abc tee /config/bilibili-helper.log
+    echo -e "今天已签到完毕=$(date +"%Y-%m-%d")" | s6-setuidgid abc tee -a /config/bilibili-helper.log
 fi
--- root/app-conf/random-bh.sh
+++ root/app-conf/random-bh.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/with-contenv bash
 
-# 该脚本默认产生6点-24点随机运行时间
+# 该脚本默认产生6点-23点随机运行时间
 # 配置签到时间范围
 STARTTIME=6
-ENDTIME=24
+ENDTIME=23
 
 # 随机数 产生随机分钟和小时
 DIFF=$(($ENDTIME-$STARTTIME))
@@ -11,8 +11,8 @@ RANDMINUTE=$(($RANDOM%60))
 RANDHOUR=$(($RANDOM%$DIFF+$STARTTIME))
 
 # 创建定时任务脚本
-cat > /config/bh-crontab << EOF
+s6-setuidgid abc cat > /config/bh-crontab << EOF
 # do daily/weekly/monthly maintenance
 # min   hour    day     month   weekday command
-  $[RANDMINUTE]     $[RANDHOUR]       *       *       *      /app-conf/bh-run.sh
+$[RANDMINUTE]     $[RANDHOUR]       *       *       *      /app-conf/bh-run.sh
 EOF
